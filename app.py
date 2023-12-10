1cfa41a51967bf9ca990e52f43f1489982d5255d
himanshu-skid19 <114365148+himanshu-skid19@users.noreply.github.com>
2023-12-10 17:27:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ from imports import *
 from pipelines import *
 from hal_check import *
 from prompts import *
-from prompt_templates import *
+from prompt_templates import * 
 from retrieval import *
 from all_apis import *
 from prompts import *
 from update_api_toolset import *
-
 warnings.filterwarnings('ignore')
 
 # retrieval examples
-retrieval_loader = CSVLoader(file_path='/content/Seed_Dataset.csv', source_column='QUERY')
+retrieval_loader = CSVLoader(file_path='/content/Seed_Dataset.csv', source_column = 'QUERY')
 retrieval_data = retrieval_loader.load()
 retrieval_embeddings = HuggingFaceEmbeddings()
 retrieval_vector_db = FAISS.from_documents(
@@ -20,19 +19,18 @@ retrieval_vector_db = FAISS.from_documents(
 )
 
 arg_allowed_values_dict = {'works-update/priority': ['p0', 'p1', 'p2', 'p3'],
-                           'works-update/type': ['issue', 'task', 'ticket'],
-                           'works_list/issue.priority': ['p0', 'p1', 'p2', 'p3'],
-                           'works_list/ticket.needs_response': ['true', 'false'],
-                           'works_list/ticket.severity': ['blocker', 'low', 'medium', 'high'],
-                           'works_list/type': ['issue', 'task', 'ticket'],
-                           'works-create/issue.priority': ['p0', 'p1', 'p2', 'p3'],
-                           'works-create/type': ['issue', 'task', 'ticket'],
-                           'works-create/title': ['issue', 'ticket']}
+ 'works-update/type': ['issue', 'task', 'ticket'],
+ 'works_list/issue.priority': ['p0', 'p1', 'p2', 'p3'],
+ 'works_list/ticket.needs_response': ['true', 'false'],
+ 'works_list/ticket.severity': ['blocker', 'low', 'medium', 'high'],
+ 'works_list/type': ['issue', 'task', 'ticket'],
+ 'works-create/issue.priority': ['p0', 'p1', 'p2', 'p3'],
+ 'works-create/type': ['issue', 'task', 'ticket'],
+ 'works-create/title': ['issue', 'ticket']}
 
 # Initialize session state for messages if not already present
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 
 # Function to clear the session state variable
 def clear_api_list_updated():
@@ -59,6 +57,7 @@ if page == "Chatbot":
         with open(file_path, 'r') as file:
             api_list = json.load(file)
 
+
         tools_ = [tool['name'] for tool in api_list]
         args_ = []
         for tool in api_list:
@@ -78,8 +77,7 @@ if page == "Chatbot":
         query = prompt
         with st.chat_message("assistant"):
             message_placeholder = st.empty()
-            json_answer = pipeline(query, api_list, args_, tools_, allowed_args_dict,
-                                   retrieval_vector_db)  # allowed args dict ka placeholder modify karna bacha
+            json_answer = pipeline(query, api_list, args_, tools_, allowed_args_dict, retrieval_vector_db) # allowed args dict ka placeholder modify karna bacha
             full_response = json_answer
             message_placeholder.markdown(full_response)
         st.session_state.messages.append({"role": "assistant", "content": full_response})
@@ -92,7 +90,7 @@ elif page == "Tool Management":
     # Load or initialize the API list in session state
     if "api_list_updated" not in st.session_state:
         st.session_state.api_list_updated = API_LIST
-
+    
     # Button to trigger the clearing action
     if st.button("Reset API list"):
         clear_api_list_updated()
@@ -104,17 +102,14 @@ elif page == "Tool Management":
         new_tool_desc = st.text_area("Tool Description")
         submitted = st.form_submit_button("Add Tool")
         if submitted:
-            st.session_state.api_list_updated = add_tool(st.session_state.api_list_updated, new_tool_name,
-                                                         new_tool_desc)
+            st.session_state.api_list_updated = add_tool(st.session_state.api_list_updated, new_tool_name, new_tool_desc)
             st.success("Tool Added Successfully!")
 
     # Update or Delete Tools
     st.header("Update or Delete Tools")
     if st.session_state.api_list_updated:
-        selected_tool_name = st.selectbox("Select a tool",
-                                          options=[tool["name"] for tool in st.session_state.api_list_updated])
-        selected_tool = next((tool for tool in st.session_state.api_list_updated if tool['name'] == selected_tool_name),
-                             None)
+        selected_tool_name = st.selectbox("Select a tool", options=[tool["name"] for tool in st.session_state.api_list_updated])
+        selected_tool = next((tool for tool in st.session_state.api_list_updated if tool['name'] == selected_tool_name), None)
 
         if selected_tool:
             with st.form("update_delete_tool_form"):
@@ -125,20 +120,18 @@ elif page == "Tool Management":
                 delete_button = st.form_submit_button("Delete Tool")
 
                 if update_button:
-                    st.session_state.api_list_updated = update_tool(st.session_state.api_list_updated,
-                                                                    selected_tool_name, new_name, new_description)
+                    st.session_state.api_list_updated = update_tool(st.session_state.api_list_updated, selected_tool_name, new_name, new_description)
                     st.success("Tool Updated Successfully!")
 
                 if delete_button:
-                    st.session_state.api_list_updated = delete_tool(st.session_state.api_list_updated,
-                                                                    selected_tool_name)
+                    st.session_state.api_list_updated = delete_tool(st.session_state.api_list_updated, selected_tool_name)
                     st.success("Tool Deleted Successfully!")
+
 
     # Add a New Argument to a Tool
     st.header("Add a New Argument to a Tool")
     if st.session_state.api_list_updated:  # Check if there are tools available
-        selected_tool_name = st.selectbox("Select a tool to add an argument",
-                                          options=[tool["name"] for tool in st.session_state.api_list_updated])
+        selected_tool_name = st.selectbox("Select a tool to add an argument", options=[tool["name"] for tool in st.session_state.api_list_updated])
         with st.form("new_argument_form"):
             new_arg_name = st.text_input("Argument Name")
             new_arg_desc = st.text_area("Argument Description")
@@ -146,29 +139,26 @@ elif page == "Tool Management":
             new_arg_type = st.text_input("Argument Type")
             submitted_arg = st.form_submit_button("Add Argument")
             if submitted_arg:
-                st.session_state.api_list_updated = add_argument(st.session_state.api_list_updated, selected_tool_name,
-                                                                 new_arg_name, new_arg_desc, new_arg_type)
+                st.session_state.api_list_updated = add_argument(st.session_state.api_list_updated, selected_tool_name, new_arg_name, new_arg_desc, new_arg_type)
                 arg_allowed_values_dict[f"{selected_tool_name}/{new_arg_name}"] = new_arg_allowed_values
                 st.success("Argument Added Successfully!")
     else:
         st.write("No tools available. Add a tool first.")
 
+
     # Update or Delete Arguments
     st.header("Update or Delete Arguments")
     if selected_tool:
         st.write(f"Selected Tool: {selected_tool_name}")
-        selected_arg_name = st.selectbox("Select an argument",
-                                         options=[arg["argument_name"] for arg in selected_tool["arguments"]])
-        selected_arg = next((arg for arg in selected_tool['arguments'] if arg['argument_name'] == selected_arg_name),
-                            None)
+        selected_arg_name = st.selectbox("Select an argument", options=[arg["argument_name"] for arg in selected_tool["arguments"]])
+        selected_arg = next((arg for arg in selected_tool['arguments'] if arg['argument_name'] == selected_arg_name), None)
 
         if selected_arg:
             with st.form("update_delete_arg_form"):
                 new_arg_name = st.text_input("New Argument Name", value=selected_arg['argument_name'])
                 new_arg_desc = st.text_area("New Argument Description", value=selected_arg['argument_description'])
-                if f"{selected_tool_name}/{selected_arg_name}" in arg_allowed_values_dict:
-                    new_arg_allowed_values = st.text_area("New Argument Allowed Values", value=arg_allowed_values_dict[
-                        f"{selected_tool_name}/{selected_arg_name}"])
+                if f"{selected_tool_name}/{selected_arg_name}" in arg_allowed_values_dict:    
+                    new_arg_allowed_values = st.text_area("New Argument Allowed Values", value=arg_allowed_values_dict[f"{selected_tool_name}/{selected_arg_name}"])
                 else:
                     new_arg_allowed_values = st.text_area("New Argument Allowed Values")
                 new_arg_type = st.text_input("New Argument Type", value=selected_arg['argument_type'])
@@ -176,15 +166,12 @@ elif page == "Tool Management":
                 delete_arg_button = st.form_submit_button("Delete Argument")
 
                 if update_arg_button:
-                    st.session_state.api_list_updated = update_argument(st.session_state.api_list_updated,
-                                                                        selected_tool_name, selected_arg_name,
-                                                                        new_arg_name, new_arg_desc, new_arg_type)
+                    st.session_state.api_list_updated = update_argument(st.session_state.api_list_updated, selected_tool_name, selected_arg_name, new_arg_name, new_arg_desc, new_arg_type)
                     arg_allowed_values_dict[f"{selected_tool_name}/{new_arg_name}"] = new_arg_allowed_values
                     st.success("Argument Updated Successfully!")
 
                 if delete_arg_button:
-                    st.session_state.api_list_updated = delete_argument(st.session_state.api_list_updated,
-                                                                        selected_tool_name, selected_arg_name)
+                    st.session_state.api_list_updated = delete_argument(st.session_state.api_list_updated, selected_tool_name, selected_arg_name)
                     st.success("Argument Deleted Successfully!")
 
     # Delete Multiple Arguments
@@ -193,10 +180,9 @@ elif page == "Tool Management":
         all_arg_names = [arg["argument_name"] for arg in selected_tool["arguments"]]
         selected_args_to_delete = st.multiselect("Select arguments to delete from the tool", options=all_arg_names)
         if st.button("Delete Selected Arguments"):
-            st.session_state.api_list_updated = delete_multiple_arguments(st.session_state.api_list_updated,
-                                                                          selected_tool_name, selected_args_to_delete)
+            st.session_state.api_list_updated = delete_multiple_arguments(st.session_state.api_list_updated, selected_tool_name, selected_args_to_delete)
             st.success("Selected Arguments Deleted Successfully!")
-
+    
     # Display the current API list
     st.header("Current API List")
     st.text(json.dumps(st.session_state.api_list_updated, indent=2))
