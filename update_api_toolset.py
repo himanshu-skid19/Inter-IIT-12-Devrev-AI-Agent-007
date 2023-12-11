445a6061e5ab6c40ba9270a4a1b3886dfbfdccf9
Parth-Agarwal216 <118837763+Parth-Agarwal216@users.noreply.github.com>
2023-12-12 01:48:52 +0530

    Update update_api_toolset.py

--- update_api_toolset.py
+++ update_api_toolset.py
@@ -114,7 +114,7 @@ def update_argument(api_list, tool_name, old_arg_name, new_arg_name, new_arg_des
     if f"{tool_name}/{old_arg_name}" in args_in_list_dict:
       del args_in_list_dict[f"{tool_name}/{old_arg_name}"]
     if len(new_arg_allowed_values) is not 0:
-      arg_allowed_values_dict[f'{tool_name}/{new_arg_name}'] = eval(new_arg_allowed_values)
+      arg_allowed_values_dict[f'{tool_name}/{new_arg_name}'] = ast.literal_eval(new_arg_allowed_values)
     if 'array' in new_arg_type.lower() :
       args_in_list_dict[f'{tool_name}/{new_arg_name}'] = 1
     else:
